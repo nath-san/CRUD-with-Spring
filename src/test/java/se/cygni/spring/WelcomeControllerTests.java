8cdb2a14664a0871d2e8845f2608b9ff267e1357
Nathalie Sandström <nathalie@NathaliesLaptop.(none)>
2010-06-09 13:27:14 +0200

    initial commit

---
+++ src/test/java/se/cygni/spring/WelcomeControllerTests.java
@@ -0,0 +1,14 @@
+package se.cygni.spring;
+
+import org.junit.Test;
+
+public class WelcomeControllerTests {
+	
+	private WelcomeController controller = new WelcomeController();
+	
+	@Test
+	public void welcome() {
+		controller.welcome();
+	}
+	
+}
